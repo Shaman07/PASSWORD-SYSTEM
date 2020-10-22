6270b6098d38f2626dc30ab27a9cdf1b8a531bec
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-10-22 11:14:35 +0530

    Add files via upload

---
+++ PASSWORD.py
@@ -0,0 +1,8 @@
+a = input("enter password:")
+print("checking")
+if a=="POMBEY":
+    print("https://github.com/THE-GREAT-CODERS")
+    print("welcome")
+else:
+    print("Fatal: authentication failed")
+    
