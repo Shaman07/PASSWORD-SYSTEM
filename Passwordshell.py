7bc826a151f058db308016dbb2ddabb607d4b965
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-10-22 11:20:51 +0530

    Add files via upload

---
+++ Passwordshell.py
@@ -0,0 +1,26 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/Dr.sudheer Rai/Desktop/PASSWORD.py =============
+enter password:POMBEY
+Traceback (most recent call last):
+  File "C:/Users/Dr.sudheer Rai/Desktop/PASSWORD.py", line 1, in <module>
+    a = int(input("enter password:"))
+ValueError: invalid literal for int() with base 10: 'POMBEY'
+>>> 
+============= RESTART: C:/Users/Dr.sudheer Rai/Desktop/PASSWORD.py =============
+enter password:
+============= RESTART: C:/Users/Dr.sudheer Rai/Desktop/PASSWORD.py =============
+enter password:POMBEY
+POMBEY
+HI
+>>> 
+============= RESTART: C:/Users/Dr.sudheer Rai/Desktop/PASSWORD.py =============
+enter password:Word
+Word
+>>> 
+============= RESTART: C:/Users/Dr.sudheer Rai/Desktop/PASSWORD.py =============
+enter password:Word
+checking
+Fatal: authentication failed
+>>> 
